8738f28f95332e9d49e387ec592a133a1feaf243
AllenTsai1215 <allen12151215@gmail.com>
2024-03-27 11:46:22 +0800

    week06

---
+++ week06/week06-4.cpp
@@ -0,0 +1,14 @@
+int maxFrequencyElements(int* nums, int numsSize) {
+    int a[101]={};//参璸计瞷Ω计秨﹍琌0(–皚柑常穦干0)
+    int best=0;
+    for(int i=0;i<numsSize;i++){
+        int now=nums[i];
+        a[now]++;
+        if(a[now]>best) best=a[now];
+    }
+    int ans=0;
+    for(int i=1;i<=100;i++){
+        if(a[i]==best) ans+=a[i];
+    }
+    return ans;
+}
